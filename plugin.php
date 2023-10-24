37a4d823306595a2c1f1ca24b0cd334f63f49861
Arseniy Olevskiy <arsidian@gmail.com>
2023-10-24 16:18:09 +0400

    regular commit

--- plugin.php
+++ plugin.php
@@ -10,6 +10,8 @@ License: GPLv2 or later
 Text Domain: ci_home_loan_calculator
 */
 
+if (!defined('ABSPATH')) exit;
+
 if (!function_exists('add_shortcode')) return "No direct call for Home Loan Calculator by Calculator.iO";
 
 function display_ci_home_loan_calculator(){
